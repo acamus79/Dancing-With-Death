d245967b6d3d67d4b87303f6dc72f470c1b9f9e7
acamus79 <adrianedgardocamus@gmail.com>
2022-05-04 15:29:07 -0300

    Cross Origin

--- JavaAPI-REST/src/main/java/com/aec/dwd/controller/DateController.java
+++ JavaAPI-REST/src/main/java/com/aec/dwd/controller/DateController.java
@@ -14,6 +14,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,6 +31,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author Adrian E. Camus
  */
 @RestController
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST})
 @RequestMapping("/dwd")
 public class DateController {
     

b57d6562fc87d535fc099274fa4ffcf7a3c094b8
acamus79 <adrianedgardocamus@gmail.com>
2022-05-05 17:28:11 -0300

    Update dwd_post.js

--- FrontEnd/js/dwd_post.js
+++ FrontEnd/js/dwd_post.js
@@ -1,28 +1,32 @@
 //const app = document.querySelector('.container');
-//const url = 'http://localhost:8080/dwd/new';
+const url = 'http://localhost:8080/dwd/new';
 
-function post_to_url(path, params, method) {
-    method = method || "post";
+const btnDay = document.querySelector("#dwdBtn");
+btnDay.onclick = postAppointment; 
 
-    var form = document.createElement("form");
+function postAppointment(){
 
-    //Move the submit function to another variable
-    //so that it doesn't get overwritten.
-    form._submit_function_ = form.submit;
+    var f = document.querySelector("#inputDate").value;
+    var day = f[8]+f[9]+"/"+f[5]+f[6]+"/"+f[0]+f[1]+f[2]+f[3];
 
-    form.setAttribute("method", method);
-    form.setAttribute("action", path);
+    var time = document.querySelector("#selectTime");
 
-    for(var key in params) {
-        var hiddenField = document.createElement("input");
-        hiddenField.setAttribute("type", "hidden");
-        hiddenField.setAttribute("name", key);
-        hiddenField.setAttribute("value", params[key]);
+    fetch(url,{
+        headers: {
+            'Accept': 'application/json, text/plain, */*',
+            'Content-Type': 'application/json'
+        },
+        method: 'POST',
+        body: JSON.stringify({dancingDate: day,
+                              dancingTime: time,
+                              name:	nam,
+                              email: mail
+                            })
+        }).then(res => res.json()).then(data => {
+            
+        })
+    .catch(err => console.log(err));
 
-        form.appendChild(hiddenField);
-    }
 
-    document.body.appendChild(form);
-    form._submit_function_(); //Call the renamed function.
+    document.getElementById("textoEncriptado").style.background = "none";
 }
-post_to_url("http://google.com/", { submit: "submit" } ); //Works!

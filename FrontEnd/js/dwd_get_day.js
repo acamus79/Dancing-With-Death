da5b0a57ca21ddf78e9a6380990e182389905208
acamus79 <adrianedgardocamus@gmail.com>
2022-05-05 15:28:38 -0300

    Update dwd_get_day.js

--- FrontEnd/js/dwd_get_day.js
+++ FrontEnd/js/dwd_get_day.js
@@ -6,12 +6,16 @@ const url = 'http://localhost:8080/dwd/date/';
 //btnDay.onclick = getDay; 
 
 $(document).ready(function(){
+    
     $('#fecha').change(function(){
-    //se asigna un id al input date y se obtiene el contenido
+    
+        $("#tb> tbody"). empty()
+
+        //se asigna un id al input date y se obtiene el contenido
         var f = $('#fecha').val();
     //Se da el formato de acuerdo a las posiciones
         day = f[8]+f[9]+"/"+f[5]+f[6]+"/"+f[0]+f[1]+f[2]+f[3];
-    
+        
         //alert(day);
 
         fetch(url,{
@@ -27,6 +31,8 @@ $(document).ready(function(){
             data.forEach(api => {
                 //var y = new Date().getFullYear();//para obtener el año actual
                 var table = document.getElementById('miTabla')//genero un elemento html segun su Id
+                
+
                     for (var i = 0; i < data.length; i++)
                     {
                         var fila = 
